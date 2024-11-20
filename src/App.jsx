26ea60951d4fe6e9378ecb89f21f28fbbaded2ca
Satolh <santiitoledo7@gmail.com>
2024-11-20 12:38:09 -0300

    Cambios en el estilo del footer y cambio del icono de la ventana

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,6 @@
 import React, { useEffect, useState } from 'react';
 import { IoMdClose } from "react-icons/io";
+import { FaXTwitter,FaInstagram,FaGithub, FaTwitter } from "react-icons/fa6";
 
 const App = () => {
   const [setContinente, continente] = useState(["North America, South America, Europe, Asia, Africa, Oceania"])
@@ -278,7 +279,19 @@ const App = () => {
           </section>
         }
 
-      <footer className='footer'></footer>
+<footer className='footer'>
+
+<div className='container-made'>
+    <p className='made'>  Made by Satolh </p>
+</div>
+<div className='container-redes'>
+  <a  className='a-icon-redes' href='#'><FaInstagram className='icon-redes' /></a>
+  <a  className='a-icon-redes' href='#'><FaTwitter className='icon-redes' /></a>
+  <a target='_blank' className='a-icon-redes' href='https://github.com/satolh'><FaGithub className='icon-redes'  /></a>
+</div>
+
+</footer>
+      
     </div>
   );
 }
